f200e693d27b6de5dcc0e1179f7f90c9fdd9683b
Shashikant <shashikant.jagtap@icloud.com>
2019-02-13 07:45:27 +0000

    Update main.swift

--- Sources/VaporWeb/main.swift
+++ Sources/VaporWeb/main.swift
@@ -4,7 +4,7 @@ var services = Services.default()
 services.register(MiddlewareConfig())
 
 let router = EngineRouter.default()
-router.get() { _ in "Hello Docker" }
+router.get() { _ in "Hello There, We are deploying Swift Web App on Kubernetes Cluster" }
 services.register(router, as: Router.self)
 
 try Application(
